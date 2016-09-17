c627ea9c1f0bd86b49335964c904581ad53d3b83
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-17 14:58:28 +0900

    add users#update_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    current_user.update(update_params)
+    redirect_to root_path,notice: 'プロフィールの編集が完了しました'
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name,:email,:member,:profile,:works)
+  end
+
 end
