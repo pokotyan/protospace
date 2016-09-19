eacdca48ade8dcfb01e8b00227356bd9576431df
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-19 11:28:51 +0900

    Add Function for Multiple images save

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,23 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+    @prototype.images.build
   end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to root_path
+  end
+
+  private
+    def create_params
+      params.require(:prototype).permit(
+        :title,
+        :catch_copy,
+        :concept,
+        images_attributes: [:image]
+      ).merge(user_id: current_user.id)
+    end
 
 end
