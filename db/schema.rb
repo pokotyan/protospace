69997392fab2f07c9f2f9dc9850ea90448a1dd24
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-20 18:54:45 +0900

    add column to images for enum

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917090315) do
+ActiveRecord::Schema.define(version: 20160920093255) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
     t.string   "image",        limit: 255
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "status",       limit: 4
   end
 
   create_table "prototypes", force: :cascade do |t|
