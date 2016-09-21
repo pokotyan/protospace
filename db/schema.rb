c3104ee4f8c969e23da9dc183d85f072056e8ec1
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-21 15:19:00 +0900

    Change_data_type integer to references

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920093255) do
+ActiveRecord::Schema.define(version: 20160921061256) do
 
   create_table "images", force: :cascade do |t|
-    t.integer  "prototype_id", limit: 4
     t.string   "image",        limit: 255
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "status",       limit: 4
+    t.integer  "prototype_id", limit: 4
   end
+
+  add_index "images", ["prototype_id"], name: "index_images_on_prototype_id", using: :btree
 
   create_table "prototypes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 20160920093255) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "images", "prototypes"
 end
