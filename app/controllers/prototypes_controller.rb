4d1f6b815696feba33d39f2645e7e1ad9a23da02
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-23 22:03:46 +0900

    add function for prototype_delete_update(未完了)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,15 +14,42 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(create_params)
     if @prototype.save
-      redirect_to root_path, notice: "プトロタイプの投稿が完了しました"
+      redirect_to root_path, notice: "プロトタイプの投稿が完了しました"
     else
       flash.now[:alert] = "プロトタイプの投稿に失敗しました"
       render :new
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+    @main_image = @prototype.images.main
+    @sub_images = @prototype.images.sub
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(update_params)
+    redirect_to root_path, notice: "プロトタイプの更新が完了しました"
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path, notice: "プロトタイプの削除が完了しました"
+  end
+
   private
     def create_params
+      params.require(:prototype).permit(
+        :title,
+        :catch_copy,
+        :concept,
+        images_attributes: [:image,:status]
+      ).merge(user_id: current_user.id)
+    end
+
+    def update_params
       params.require(:prototype).permit(
         :title,
         :catch_copy,
