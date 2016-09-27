4d5ffd3423ef37070c147bd75f2ad8dbbc853eea
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-27 10:18:46 +0900

    add action to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.build(create_params)
+    @comment.save
+    @prototype = @comment.prototype
+    @comments = Comment.where(prototype_id:@prototype)
+  end
+
+  private
+
+  def create_params
+    params.require(:comment).permit(:content,:prototype_id)
+  end
+
 end
