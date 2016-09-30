c26e692e3d58d6622ccde05524b8818566082c60
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-30 18:02:55 +0900

    use helper_method for switching li.active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def popular_index_active?(controller_name)
+    if controller.controller_name == "popular"
+      "active"
+    end
+  end
+
+  def newest_index_active?(controller_name)
+    if controller.controller_name == "newest"
+      "active"
+    end
+  end
+
 end
