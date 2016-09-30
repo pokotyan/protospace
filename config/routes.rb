9e8cad46f1e4e644ee715117f6aaa87bfda30c7d
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-09-30 14:30:03 +0900

    Add Update order sort_function to prototypes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  #resources :prototypesより上にこのルーティングがないとprototypes/newestへのアクセスが
+  #prototype_path(/prototypes/:id)へのアクセスと解釈されるため、意図した動作にならなくなる。
+  get 'prototypes/newest'  => 'prototypes/newest#index'
+
   resources :prototypes
   resources :users
   resources :likes, only: [:create, :destroy]
