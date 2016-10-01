7dbd956d74c4e9bf403af72eefd650fcc19d3e8f
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-01 19:31:01 +0900

    fix for tags_index_view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-   @web_design_tags_count = Prototype.tags_on(:web_designs).pluck(:taggings_count).inject(:+)
-   @application_tags_count = Prototype.tags_on(:applications).pluck(:taggings_count).inject(:+)
-   @uis_tags_count = Prototype.tags_on(:uis).pluck(:taggings_count).inject(:+)
+    @tags = ActsAsTaggableOn::Tag.all
   end
 
   def show
