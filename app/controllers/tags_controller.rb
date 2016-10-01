2c66e4a43314b780aa97f774eda5391d3b7b3fc9
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-01 19:33:02 +0900

    fix for tags_show_view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,8 @@ class TagsController < ApplicationController
   end
 
   def show
+    @tag_name = ActsAsTaggableOn::Tag.find(params[:id]).name
+    @tags = Prototype.tagged_with(@tag_name)
   end
 
 end
