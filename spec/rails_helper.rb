661b7f1c76bc90fc8b2eedd8b4ddd67495b2fc74
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-12 17:12:53 +0900

    setting change
    --format documentation
    　rspecの出力をデフォルトから読みやすいドキュメント形式に変更
    
    config.include FactoryGirl::Syntax::Methods
    　テストを書く際、FactoryGirlの記述を省略するため
    
    Capybara.javascript_driver = :poltergeist
    　Capybaraの標準のwebドライバは、Rack::Testというもので、このドライバはJavaScriptを実行しないため、JavaScriptを無視されます。
    　Javasciptを使用しているProtospaceにはJavaScriptの実行可能なwebドライバが必要となります。
    　そこで、JavaScriptに対応し、かつheadless(webブラウザを起動することなくテストを行う)なpoltergeistというgemを使用します。
    
    config.use_transactional_fixtures = false
    　この設定がtrueになっている場合、すべてのテストはトランザクション中で実行される。
    　なので、create(:user)もトランザクション中で実行される。
    　Capybaraでvisit root_pathしても、create(:user)のトランザクションはコミットされていないため、レコードが作成されておらず、テスト失敗の原因になる。
    　use_transactional_fixturesをfalseにするとレコードが作成されるようになる。
    　ただし、その場合はテストで作成したレコードを削除するなどの処理を自分で管理する必要があるため、
    　database_cleanerをつかい、テストで作成したレコードを削除する
    
    config.before(:suite) do
    　DatabaseCleaner.clean_with(:truncation)
    テストスイート全体が実行される前にテストデータベースを完全に消去します。
    これにより、中断したテストや下手なテストコードによって生じたゴミを一掃（トランケート）し、テストが思わぬ振る舞いをするのを防ぎます。
    
    DatabaseCleaner.strategy = :transaction
    　上の設定は、データベース消去のストラテジを”transaction”に設定します。トランザクションは非常に速いので、
    　トランザクションを実行するすべてのテスト(実際のところ、RSpecプロセスで実行されるすべてのテスト)において、この設定を行っておくのがよいでしょう。
    
    config.around(:each) do |example|
        DatabaseCleaner.cleaning do
          example.run
    トランザクションの開始と終了を実行するタイミング(各 example を実行する前後)でデータ削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/poltergeist'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,10 +31,13 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include FactoryGirl::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +58,16 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # database_cleaner関連のセットアップ
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
