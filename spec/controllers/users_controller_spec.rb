21563b9e82a1f2596c3923136f4bcfe7a632488b
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-21 17:48:45 +0900

    add test PATCH #update for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -37,5 +37,34 @@ describe UsersController do
 
     end
 
+    describe 'PATCH #update' do
+
+      before :each do
+        patch :update, id: @user, user: attributes_for(:user,name: "update_name")
+        @user.reload
+      end
+
+      #current_userが@userと等しいこと。（ユーザー編集できるのはログインしている自分自身のみ）
+      it "assigns the requested to user to @user" do
+        expect(assigns(:current_user)).to eq @user
+      end
+
+      #更新できること
+      it "change @user's attributes" do
+        expect(@user.name).to eq "update_name"
+      end
+
+      #rootページにリダイレクトすること
+      it "redirect to prototype_path" do
+        expect(response).to redirect_to root_path
+      end
+
+      #更新完了のflashメッセージが出ること
+      it "shows flash message to update user successfully" do
+        expect(flash[:notice]).to eq 'プロフィールの編集が完了しました'
+      end
+
+    end
+
   end
 end
