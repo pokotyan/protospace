25dc100db4e82d60603ab19949a226b55cb39842
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-21 17:34:29 +0900

    add test GET #edit for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,17 +4,17 @@ require 'pry-rails'
 describe UsersController do
 
   describe 'with user login' do
-    let(:user){ create(:user) }
+    login_user
 
     describe 'GET #show' do
 
       before :each do
-        get :show, id: user
+        get :show, id: @user
       end
 
       #要求されたuserが@userに格納されていること
       it "assigns the requested to @user" do
-        expect(assigns(:user)).to eq user
+        expect(assigns(:user)).to eq @user
       end
 
       #showテンプレートを表示すること
@@ -23,5 +23,19 @@ describe UsersController do
       end
 
     end
+
+    describe 'GET #edit' do
+
+      before :each do
+        get :edit, id: @user
+      end
+
+      #editテンプレートを表示すること
+      it "renders the :edit template" do
+        expect(response).to render_template :edit
+      end
+
+    end
+
   end
 end
