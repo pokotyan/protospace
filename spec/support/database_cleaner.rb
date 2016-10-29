bc0e84048700d2a3c1459a5d39f461068fcb03de
pokotyan <tomohiro_soga@yahoo.co.jp>
2016-10-29 14:14:04 +0900

    add test post_prototype

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -4,9 +4,16 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
   end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
